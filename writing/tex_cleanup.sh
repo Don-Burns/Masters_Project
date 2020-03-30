74205084639ee3bd52d0f96dad929d2b1ac0d0fc
Donal Burns <donal.w.burns@hotmail.com>
2020-03-30 14:04:58 +0100

    Added project proposal

---
+++ writing/tex_cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# to remove the "trash" files generated when compiling latex `.tex` files
+
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.out
+rm *.toc
+rm *.dvi
+rm *.gz
+rm *.fls
+rm *.fdb_latexmk
+rm *.synctex.gz
