b83df77de95add539659a3e8efa95c3f3900747e
Don-Burns <donal.w.burns@hotmail.com>
2020-08-19 16:23:57 +0100

    Changed name due again due to format mistake

--- writing/thesis/Compile_Report.sh
+++ writing/thesis/Compile_Report.sh
@@ -4,13 +4,13 @@
 # Date: 26/05/2020
 ###############################################################################
 # compile the report
-texcount -1 -sum Donal_Burns_CMEE_MSc_2020.tex > ./Donal_Burns_CMEE_MSc_2020.sum # word count
+texcount -1 -sum Burns_Donal_CMEE_MSc_2020.tex > ./Burns_Donal_CMEE_MSc_2020.sum # word count
 sleep 2s
-pdflatex Donal_Burns_CMEE_MSc_2020.tex
-pdflatex Donal_Burns_CMEE_MSc_2020.tex
-biber Donal_Burns_CMEE_MSc_2020
-pdflatex Donal_Burns_CMEE_MSc_2020.tex
-pdflatex Donal_Burns_CMEE_MSc_2020.tex
+pdflatex Burns_Donal_CMEE_MSc_2020.tex
+pdflatex Burns_Donal_CMEE_MSc_2020.tex
+biber Burns_Donal_CMEE_MSc_2020.tex
+pdflatex Burns_Donal_CMEE_MSc_2020.tex
+pdflatex Burns_Donal_CMEE_MSc_2020.tex
 # remove junk files produced 
 sleep 5s # force a pause to ensure the files are removed, without they are left behind for some reason
 
