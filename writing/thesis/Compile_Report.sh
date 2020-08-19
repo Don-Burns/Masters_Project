2f2b4026fa20eb966489aa53382fb6b3cba20387
Don-Burns <donal.w.burns@hotmail.com>
2020-08-19 15:01:48 +0100

    Renamed report.* to Donal_Burns_CMEE_MSc2020.*

--- writing/thesis/Compile_Report.sh
+++ writing/thesis/Compile_Report.sh
@@ -4,13 +4,13 @@
 # Date: 26/05/2020
 ###############################################################################
 # compile the report
-texcount -1 -sum Report.tex > ./Report.sum # word count
+texcount -1 -sum Donal_Burns_CMEE_MSc_2020.tex > ./Donal_Burns_CMEE_MSc_2020.sum # word count
 sleep 2s
-pdflatex Report.tex
-pdflatex Report.tex
-biber Report
-pdflatex Report.tex
-pdflatex Report.tex
+pdflatex Donal_Burns_CMEE_MSc_2020.tex
+pdflatex Donal_Burns_CMEE_MSc_2020.tex
+biber Donal_Burns_CMEE_MSc_2020
+pdflatex Donal_Burns_CMEE_MSc_2020.tex
+pdflatex Donal_Burns_CMEE_MSc_2020.tex
 # remove junk files produced 
 sleep 5s # force a pause to ensure the files are removed, without they are left behind for some reason
 
--- writing/thesis/housekeeping.sh
+++ writing/thesis/housekeeping.sh
@@ -1,3 +1,3 @@
-texcount -1 -sum Report.tex > ./Report.sum # word count
+texcount -1 -sum Donal_Burns_CMEE_MSc_2020.tex > ./Donal_Burns_CMEE_MSc_2020.sum # word count
 # remove junk files 
 bash ../tex_cleanup.sh ./
